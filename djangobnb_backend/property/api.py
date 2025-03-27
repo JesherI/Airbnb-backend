bf3e10274dcd8fe547e11f11c1c868453b265afe
Jeshersin <jeshermendieta@gmail.com>
2025-03-26 21:27:04 -0600

    08 version add landlord

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -11,6 +11,12 @@ from .serializers import PropertyesListSerializer, PropertyDetailSerializer, Res
 @permission_classes([])
 def properties_list(request):
     properties = Property.objects.all()
+
+    landlord_id = request.GET.get('landlord_id', '')
+    
+    if landlord_id:
+        properties = properties.filter(landlord_id=landlord_id)
+
     serializers = PropertyesListSerializer(properties, many=True)
 
     return JsonResponse({
